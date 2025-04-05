789937f2ee749200cca8e548d64859493a921045
Prasanna-cpu <kumar22maran@gmail.com>
2025-04-05 06:42:02 +0530

    changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+import {ClerkProvider, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs";
 
 export default function Home() {
-  return (
-   <div>
-     <h1>Home Page</h1>
-   </div>
-  );
+    return (
+        <div>
+            <ClerkProvider>
+                <SignedOut>
+                    <SignInButton mode={"modal"}>
+                        <button className={"bg-red-600 p-6 mt-4 ml-4 rounded-2xl border-none"}>
+                            Sign In
+                        </button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton/>
+                </SignedIn>
+            </ClerkProvider>
+        </div>
+    );
+
 }
