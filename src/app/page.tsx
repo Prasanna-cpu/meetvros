43944b594771ed70141995a5803aae4c1ea8902b
Prasanna-cpu <kumar22maran@gmail.com>
2025-04-05 09:27:07 +0530

    Revert "changes made"
    This reverts commit 789937f2ee749200cca8e548d64859493a921045.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,8 @@
-import {ClerkProvider, SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs";
 
 export default function Home() {
-    return (
-        <div>
-            <ClerkProvider>
-                <SignedOut>
-                    <SignInButton mode={"modal"}>
-                        <button className={"bg-red-600 p-6 mt-4 ml-4 rounded-2xl border-none"}>
-                            Sign In
-                        </button>
-                    </SignInButton>
-                </SignedOut>
-                <SignedIn>
-                    <UserButton/>
-                </SignedIn>
-            </ClerkProvider>
-        </div>
-    );
-
+  return (
+   <div>
+     <h1>Home Page</h1>
+   </div>
+  );
 }
